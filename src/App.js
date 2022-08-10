40bfc524814234caec02380bf20ff312cfbda85c
unknown <6210110446@psu.ac.th>
2022-08-10 21:24:08 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WordCard from './WordCard';
+function App() {
+  return (
+    <div>
+    <WordCard value="1234"/>
+    </div>
+    );
+   
+}
+
+export default App;
